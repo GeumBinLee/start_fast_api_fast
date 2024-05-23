9b493648303ead302e8ba4ecd2cf21ae06b0d9a9
GeumBinLee <lgb9811@gmail.com>
2024-05-23 17:23:14 +0900

    feat: Environment enum 클래스 추가
    - 언어 설정을 위한 Environment enum 클래스 추가
    - 기본값 및 데이터 타입 설정 포함

---
+++ core/common/environment.py
@@ -0,0 +1,15 @@
+from enum import auto
+
+from core.common.enums import Currency, StringEnum
+
+
+class Environment(str, StringEnum):
+    LANGUAGE = (auto(), "언어", str, "korean")
+
+    def __new__(cls, value, description, data_type, default_value):
+        obj = str.__new__(cls, [value])
+        obj._value_ = value
+        obj.description = description
+        obj.data_type = data_type
+        obj.default_value = default_value
+        return obj
