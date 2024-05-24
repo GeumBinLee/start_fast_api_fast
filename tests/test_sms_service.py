384998fcd0716188e87dedb4e387a4648defc31b
GeumBinLee <lgb9811@gmail.com>
2024-05-24 14:19:00 +0900

    test: send_sms 스킵 안 하고 모의로 테스트 (커버리지: 83.45%)

--- tests/test_sms_service.py
+++ tests/test_sms_service.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch
+
 import pytest
 
 from app.core.enums import AuthType
@@ -15,7 +17,24 @@ def test_unique_id():
     assert len(uid) > 0
 
 
-@pytest.mark.skip(reason="This test sends a real SMS, use with caution")
-def test_send_sms():
-    response = send_sms("01012345678", "테스트 메시지", AuthType.VERIFICATION)
-    assert response["status"] == "SUCCESS"
+def mock_requests_post(*args, **kwargs):
+    class MockResponse:
+        def __init__(self, json_data, status_code):
+            self.json_data = json_data
+            self.status_code = status_code
+            self.text = str(json_data)  # 응답 내용을 문자열로 설정
+
+        def json(self):
+            return self.json_data
+
+    # 원하는 모의 응답 데이터 설정
+    if args[0] == "https://api.coolsms.co.kr/messages/v4/send-many":
+        return MockResponse({"status": "SENDING"}, 200)
+
+    return MockResponse(None, 404)
+
+
+@patch("requests.post", side_effect=mock_requests_post)
+def test_send_sms(mock_post):
+    response = send_sms("01043996646", "테스트 메시지", AuthType.VERIFICATION)
+    assert response["status"] == "SENDING"
